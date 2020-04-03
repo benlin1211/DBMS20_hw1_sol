65cf0b3f1b8e5d2fecbb5ed1985936f8d0b80898
karljackab <karljackab@gmail.com>
2020-04-04 00:13:44 +0800

    update preprocess.sql at participant and match_info

--- data/preprocess.sql
+++ data/preprocess.sql
@@ -33,32 +33,18 @@ where player_id in
     )
 );
 
--- delete match_info
-delete from match_info
-where match_id in
+-- delete participant
+delete from participant
+where player_id not in
 (
-    select distinct grp.match_id
-    from
-    (
-        select par.match_id, stat.win, count(stat.win) as cnt
-        from participant as par, stat
-        where par.player_id = stat.player_id
-        group by par.match_id, stat.win
-    ) as grp
-    where grp.cnt != 5
+    select player_id
+    from stat
 );
 
 -- delete match_info
-delete from participant
-where match_id in
+delete from match_info
+where match_id not in
 (
-    select distinct grp.match_id
-    from
-    (
-        select par.match_id, stat.win, count(stat.win) as cnt
-        from participant as par, stat
-        where par.player_id = stat.player_id
-        group by par.match_id, stat.win
-    ) as grp
-    where grp.cnt != 5
+    select distinct match_id
+    from participant
 );
