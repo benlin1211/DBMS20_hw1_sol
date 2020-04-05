87cbd14b386e0bf47c71517a36f207c49a0f4adb
yao0510 <sf163864@gmail.com>
2020-04-05 12:55:57 +0800

    finish 10.sql

--- answer/yao/10.sql
+++ answer/yao/10.sql
@@ -3,21 +3,33 @@
 SELECT *
 FROM
 (
-    SELECT c.champion_name as self_champ_name, SUM(s.win) / COUNT(*) as win_ratio, (SUM(s.kills) + SUM(s.assists)) / SUM(s.deaths) as self_kda, AVG(s.goldearned) as self_avg_gold, COUNT(*) as game
-    FROM participant as p, stat as s, champ as c
-    WHERE p.player_id = s.player_id
-    AND p.position = 'TOP'
-    AND p.champion_id = c.champion_id
-    AND p.match_id IN (
-                        SELECT p.match_id
-                        FROM participant as p, stat as s, champ as c
-                        WHERE p.player_id = s.player_id
-                        AND p.position = 'TOP'
-                        AND p.champion_id = c.champion_id
-                        AND c.champion_name = 'Renekton'
-                      )
-    GROUP BY c.champion_name
-    ORDER BY win_ratio DESC
-) as self_info
-WHERE self_info.game >= 100
-LIMIT 5
+    SELECT self_champ_name, SUM(self_win) / COUNT(*) as win_ratio, (SUM(self_kills)+SUM(self_assists)) / SUM(self_deaths) as self_kda, AVG(self_gold) as self_avg_gold, enemy_champ_name, (SUM(enemy_kills)+SUM(enemy_assists)) / SUM(enemy_deaths) as enemy_kda, AVG(enemy_gold) as enemy_avg_gold, COUNT(*) as game
+    FROM
+    (
+        SELECT infoA.champion_name as self_champ_name, infoA.win as self_win, infoA.kills as self_kills, infoA.assists as self_assists, infoA.deaths as self_deaths, infoA.goldearned as self_gold, infoB.champion_name as enemy_champ_name, infoB.win as enemy_win, infoB.kills as enemy_kills, infoB.assists as enemy_assists, infoB.deaths as enemy_deaths, infoB.goldearned as enemy_gold
+        FROM
+        (
+            SELECT p.match_id, p.player, c.champion_name, s.win, s.kills, s.assists, s.deaths, s.goldearned
+            FROM participant as p, stat as s, champ as c
+            WHERE p.player_id = s.player_id
+            AND p.position = 'TOP'
+            AND p.champion_id = c.champion_id
+            AND c.champion_name != 'Renekton'
+        ) as infoA
+        INNER JOIN 
+        (
+            SELECT p.match_id, p.player, c.champion_name, s.win, s.kills, s.assists, s.deaths, s.goldearned
+            FROM participant as p, stat as s, champ as c
+            WHERE p.player_id = s.player_id
+            AND p.position = 'TOP'
+            AND p.champion_id = c.champion_id
+            AND c.champion_name = 'Renekton'
+        ) as infoB
+        ON infoA.match_id = infoB.match_id
+        AND infoA.win != infoB.win
+    ) as combine
+    GROUP BY combine.self_champ_name, combine.enemy_champ_name
+) as final
+WHERE game >= 100
+ORDER BY win_ratio DESC
+LIMIT 5;
